03f8b4a8375df42dce63f0ba20660ca98bc0acdf
Utsav <ukainth6@gmail.com>
2026-08-03 23:00:23 +0530

    Remove backup and temporary files

--- backend-backup/backend/src/routes/wishlistRoutes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { protect } from "../middleware/auth.js";
-import { addWishlist, getWishlist, removeWishlist } from "../controllers/wishlistController.js";
-const router = Router();
-router.use(protect);
-router.get("/", getWishlist);
-router.post("/:productId", addWishlist);
-router.delete("/:productId", removeWishlist);
-export default router;
